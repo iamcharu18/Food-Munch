f1d4e195fb6898090e40d0255b775e9a56b7cefb
Sobhan Sai Kuriti <86838054+iamcharu18@users.noreply.github.com>
2022-11-23 16:33:48 +0530

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,11 +1,5 @@
 let firebaseConfig = {
     // Enter your firebase credentials
-    apiKey: "AIzaSyB-BWVKZPRgxjdaAYsb-7J1eqQPiWft50s",
-    authDomain: "task-swio.firebaseapp.com",
-    projectId: "task-swio",
-    storageBucket: "task-swio.appspot.com",
-    messagingSenderId: "730438407791",
-    appId: "1:730438407791:web:8f4b798370d03b11409f9a"
 };
 
 firebase.initializeApp(firebaseConfig);
